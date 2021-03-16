f4552fa65cfb47a6869c8de68f61e06844d1b8d5
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-03-16 14:54:47 +0000

    order and payment
    constants, reducers, scerens, urls, views etc added

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -9,6 +9,7 @@ from base.products import products
 from base.serializers import ProductSerializer, OrderSerializer
 
 from rest_framework import status
+from datetime import datetime
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -60,3 +61,30 @@ def addOrderItems(request):
         serializer = OrderSerializer(order, many=False)
 
         return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderByID(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail':'Not Authorised to view this Order'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+            return Response({'detail':'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request, pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid = True
+    order.paidAt = datetime.now()
+    order.save()
+    return Response('Order was paid')
