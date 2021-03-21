6a303f84fe42d55c7b5653b6328abe3c026d2542
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-03-21 11:00:11 +0000

    product management continued

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 import './App.css';
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
 					<Route path='/admin/user/:id/edit' component={UserEditScreen} />
 
 					<Route path='/admin/productlist' component={ProductListScreen} />
+					<Route path='/admin/product/:id/edit' component={ProductEditScreen} />
 				</Container>
 			</main>
 			<Footer />
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,12 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
-import { listProducts, deleteProduct } from '../actions/productActions';
+import {
+	listProducts,
+	deleteProduct,
+	createProduct,
+} from '../actions/productActions';
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 
 function ProductListScreen({ history, match }) {
 	const dispatch = useDispatch();
@@ -19,16 +24,37 @@ function ProductListScreen({ history, match }) {
 		success: successDelete,
 	} = productDelete;
 
+	const productCreate = useSelector((state) => state.productCreate);
+	const {
+		loading: loadingCreate,
+		error: errorCreate,
+		success: successCreate,
+		product: createdProduct,
+	} = productCreate;
+
 	const userLogin = useSelector((state) => state.userLogin);
 	const { userInfo } = userLogin;
 
 	useEffect(() => {
-		if (userInfo && userInfo.isAdmin) {
-			dispatch(listProducts());
-		} else {
+		dispatch({ type: PRODUCT_CREATE_RESET });
+
+		if (!userInfo.isAdmin) {
 			history.push('/login');
 		}
-	}, [dispatch, history, userInfo, successDelete]);
+
+		if (successCreate) {
+			history.push(`/admin/product/${createdProduct._id}/edit`);
+		} else {
+			dispatch(listProducts());
+		}
+	}, [
+		dispatch,
+		history,
+		userInfo,
+		successCreate,
+		successDelete,
+		createdProduct,
+	]);
 
 	const deleteHandler = (id) => {
 		if (window.confirm('Are you sure you want to delete this product?')) {
@@ -36,7 +62,9 @@ function ProductListScreen({ history, match }) {
 		dispatch(deleteProduct(id));
 	};
 
-	const createProductHandler = (product) => {};
+	const createProductHandler = (product) => {
+		dispatch(createProduct());
+	};
 
 	return (
 		<div>
@@ -53,6 +81,9 @@ function ProductListScreen({ history, match }) {
 
 			{loadingDelete && <Loader />}
 			{errorDelete && <Message variant='danger'>{error}</Message>}
+
+			{loadingCreate && <Loader />}
+			{errorCreate && <Message variant='danger'>{error}</Message>}
 
 			{loading ? (
 				<Loader />
