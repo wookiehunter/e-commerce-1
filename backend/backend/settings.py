4d46e8cc075f1646f24445dfacffa0f66fe8575b
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-03-26 15:19:55 +0000

    connected to local postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,8 +121,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ProShop',
+        'USER': 'postgres',
+        'PASSWORD': 'P@@ntang12',
+        'HOST': 'localhost',
+        'PORT': '5432'
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
